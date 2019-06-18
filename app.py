090658c6b427c3f6d9e084add13565e9fef2ef40
Alex Caruso <alex.caruso772@gmail.com>
2019-06-17 21:44:31 -0400

    send score back to client

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask_cors import CORS, cross_origin
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow.keras as keras
 
@@ -13,67 +14,60 @@ app = Flask(__name__)
 @app.route("/", methods=['GET','POST'])
 def main():
     """get board data from client, input to Keras model, and send board scores back to client"""
-    piece_list = ('p','n','b','r','q','k')
+    
+    def get_model():
+        return keras.Sequential([
+            keras.layers.Dense(2048, input_dim=768, activation=tf.nn.elu),
+            keras.layers.Dense(2048, activation=tf.nn.elu),
+            keras.layers.Dense(2048, activation=tf.nn.elu),
+            keras.layers.Dense(1)
+        ])
+
+    prediction_model = get_model()
+    prediction_model.load_weights('model_weights.h5')
+
+    piece_list = ('p','n','b','r','q','k','P','N','B','R','Q','K')
 
     if request.method == "POST":
         board_data = request.json
+        turn = board_data['turn']
+        board = board_data['board']
         feature_vector = []
 
-        for row in board_data:
+        for row in board:
             for square in row:
+                if square != None and square['color'] == 'w':
+                    square['type'] = square['type'].upper()
                 if square == None:
                     for i in range(12):
                         feature_vector.append(np.int8(0))
-                elif square['color'] == 'w': #find a way to keep track of who's move it is! Send all board positions at once???
-                    for piece in piece_list: # this is only 6 iterations, not 12!! needs to be 12 iterations per square
+                elif square['color'] == 'w' and turn == 'w':
+                    for piece in piece_list:
                         if square['type'] == piece:
                             feature_vector.append(np.int8(1))
                         else:
                             feature_vector.append(np.int8(0))
-                # { more if else conditionals }
+                elif square['color'] == 'w' and turn == 'b':
+                    for piece in piece_list:
+                        if square['type'] == piece:
+                            feature_vector.append(np.int8(-1))
+                        else:
+                            feature_vector.append(np.int8(0))
+                elif square['color'] == 'b' and turn == 'w':
+                    for piece in piece_list:
+                        if square['type'] == piece:
+                            feature_vector.append(np.int8(-1))
+                        else:
+                            feature_vector.append(np.int8(0))
+                elif square['color'] == 'b' and turn == 'b':
+                    for piece in piece_list:
+                        if square['type'] == piece:
+                            feature_vector.append(np.int8(1))
+                        else:
+                            feature_vector.append(np.int8(0))
 
-        # example logic from parser
-
-        # if square.islower() and move_num % 2 != 0: #whites move
-        #         for piece in piece_list:
-        #             if square == piece:
-        #                 feature_vector.append(np.int8(-1)) 
-        #             else:
-        #                 feature_vector.append(np.int8(0))
-        #     elif square.islower() and move_num % 2 == 0: #blacks move
-        #         for piece in piece_list:
-        #             if square == piece:
-        #                 feature_vector.append(np.int8(1))
-        #             else:
-        #                 feature_vector.append(0)
-        #     elif square.isupper() and move_num % 2 != 0: #whites move
-        #         for piece in piece_list:
-        #             if square == piece:
-        #                 feature_vector.append(np.int8(1))
-        #             else:
-        #                 feature_vector.append(0)
-        #     elif square.isupper() and move_num % 2 == 0: #blacks move
-        #         for piece in piece_list:
-        #             if square == piece:
-        #                 feature_vector.append(np.int8(-1))
-        #             else:
-        #                 feature_vector.append(np.int8(0))
-        #     else: # if the square is empty, append 12 0s (one for each piece)
-        #         for i in range(12):
-        #             feature_vector.append(np.int8(0))
-
-
-    # def get_model():
-    #     return keras.Sequential([
-    #         keras.layers.Dense(2048, input_dim=768, activation=tf.nn.elu),
-    #         keras.layers.Dense(2048, activation=tf.nn.elu),
-    #         keras.layers.Dense(2048, activation=tf.nn.elu),
-    #         keras.layers.Dense(1)
-    #     ])
-
-    # prediction_model = get_model()
-    # prediction_model.load_weights('model_weights.h5')
-    # board_score = prediction_model.predict()
+        board_score = prediction_model.predict(np.array(pd.Series(feature_vector)).reshape(1, 768))
+        return render_template('index.html', score = board_score[0][0])
 
     return render_template('index.html')
 
